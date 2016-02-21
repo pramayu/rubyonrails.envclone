7a9131e67d370487eeb8c3b8f59d04ed30a1b073
pramayugede <gedepramayu@gmail.com>
2016-02-21 22:42:04 +0700

    Create category and subcategory

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,3 +1,40 @@
 class ThemesController < ApplicationController
 
+	def index
+		
+	end
+
+	def show
+		
+	end
+
+	def new
+		
+	end
+
+	def create
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def delete
+		
+	end
+
+	private
+
+	def themes_params
+		
+	end
+
+	def find_themes
+		
+	end
 end
