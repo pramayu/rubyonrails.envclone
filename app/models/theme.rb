7e6193787aa2c4b79ce0a77e2b8aa9ed63422b2f
pramayugede <gedepramayu@gmail.com>
2016-02-22 01:27:00 +0700

    create broser, compatible and file include model & generate association many to many

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,4 +1,17 @@
 class Theme < ActiveRecord::Base
 	belongs_to :category
 	belongs_to :subcategory
+
+	# browser to themes
+	has_many :browserthemes
+	has_many :browsers, through: :browserthemes
+
+	# compatible to themes
+	has_many :compatiblethemes
+	has_many :compatibles, through: :compatiblethemes
+
+	# files include to themes
+	has_many :fileincludethemes
+	has_many :fileincludes, through: :fileincludethemes
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,61 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160221161647) do
+ActiveRecord::Schema.define(version: 20160221182407) do
+
+  create_table "browsers", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "browserthemes", force: :cascade do |t|
+    t.integer  "theme_id",   limit: 4
+    t.integer  "browser_id", limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "browserthemes", ["browser_id"], name: "index_browserthemes_on_browser_id", using: :btree
+  add_index "browserthemes", ["theme_id"], name: "index_browserthemes_on_theme_id", using: :btree
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
+
+  create_table "compatibles", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "compatiblethemes", force: :cascade do |t|
+    t.integer  "theme_id",      limit: 4
+    t.integer  "compatible_id", limit: 4
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
+  add_index "compatiblethemes", ["compatible_id"], name: "index_compatiblethemes_on_compatible_id", using: :btree
+  add_index "compatiblethemes", ["theme_id"], name: "index_compatiblethemes_on_theme_id", using: :btree
+
+  create_table "fileincludes", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "fileincludethemes", force: :cascade do |t|
+    t.integer  "theme_id",       limit: 4
+    t.integer  "fileinclude_id", limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_index "fileincludethemes", ["fileinclude_id"], name: "index_fileincludethemes_on_fileinclude_id", using: :btree
+  add_index "fileincludethemes", ["theme_id"], name: "index_fileincludethemes_on_theme_id", using: :btree
 
   create_table "subcategories", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -44,6 +92,12 @@ ActiveRecord::Schema.define(version: 20160221161647) do
   add_index "themes", ["category_id"], name: "index_themes_on_category_id", using: :btree
   add_index "themes", ["subcategory_id"], name: "index_themes_on_subcategory_id", using: :btree
 
+  add_foreign_key "browserthemes", "browsers"
+  add_foreign_key "browserthemes", "themes"
+  add_foreign_key "compatiblethemes", "compatibles"
+  add_foreign_key "compatiblethemes", "themes"
+  add_foreign_key "fileincludethemes", "fileincludes"
+  add_foreign_key "fileincludethemes", "themes"
   add_foreign_key "subcategories", "categories"
   add_foreign_key "themes", "categories"
   add_foreign_key "themes", "subcategories"
